4386a041e2e32cf8bb455d178d146665ecac99f1
vlad <vnelipovich63@gmail.com>
2023-03-12 19:16:26 +0300

    did task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,47 @@
 import React, {useState} from 'react';
-import './App.css';
 import {Input} from "./components/Input";
 import {Button} from "./components/Button";
 
+// function App() {
+//     let [message, setMessage] = useState([
+//         {message: 'message1'},
+//         {message: 'message2'},
+//         {message: 'message3'},
+//     ])
+//
+//     let [title, setTitle] = useState('')
+//     console.log(title)
+//
+//     const addMessage = (title: string) => {
+//         let newMessage = {message: title}
+//         setMessage([newMessage, ...message,])
+//     }
+//     const CallBackButtonHandler = () => {
+//         addMessage(title)
+//         setTitle('')
+//     }
+//
+//     return (
+//         <div className={'App'}>
+//             <Input setTitle={setTitle} title={title}/>
+//             <Button name={'+'} callback={CallBackButtonHandler}/>
+//             {message.map((el, index) => {
+//                 return (
+//                     <div key={index}>{el.message}</div>
+//                 )
+//             })}
+//         </div>
+//     );
+// }
+//
+// export default App;
+
 function App() {
-    let [message, setMessage] = useState([
-        {message: 'message1'},
-        {message: 'message2'},
-        {message: 'message3'},
-    ])
-
-    let [title, setTitle] = useState('')
-    console.log(title)
-
-    const addMessage = (title: string) => {
-        let newMessage = {message: title}
-        setMessage([newMessage, ...message,])
-    }
-    const CallBackButtonHandler = () => {
-        addMessage(title)
-        setTitle('')
-    }
-
     return (
-        <div className={'App'}>
-            <Input setTitle={setTitle} title={title}/>
-            <Button name={'+'} callback={CallBackButtonHandler}/>
-            {message.map((el, index) => {
-                return (
-                    <div key={index}>{el.message}</div>
-                )
-            })}
+        <div>
+
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
